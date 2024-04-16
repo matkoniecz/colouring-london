437dfa54f772aa38fc8eba818391f647bddf3723
Mateusz Konieczny <matkoniecz@gmail.com>
2024-04-16 13:06:01 +0200

    log cached tiles

--- app/src/tiles/renderers/getTileWithCaching.ts
+++ app/src/tiles/renderers/getTileWithCaching.ts
@@ -5,6 +5,7 @@ import { RendererFunction, Tile, TileParams } from "../types";
 async function getTileWithCaching(tileParams: TileParams, dataParams: any, tileCache: TileCache, renderTile: RendererFunction): Promise<Tile> {
     try {
         const tile = await tileCache.get(tileParams);
+        console.log(" getTileWithCaching called tileParams: " + tileParams + "dataParams: " + dataParams)
         return tile;
     } catch (err) {
         const im = await renderTile(tileParams, dataParams);
